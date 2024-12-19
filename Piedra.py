148e063d31b659a17bf0ffdb81af4eb29cf7f0e2
sebasflo <josfloressa@uide.edu.ec>
2024-12-19 01:25:03 +0000

    ULTIMOS CANBIOS

--- Piedra.py
+++ Piedra.py
@@ -1,24 +1,51 @@
 import random
 
 list = ("piedra","papel","tijera")
-
-COMPUTADORA = random.choice(list)
-JUGADOR = None
-
-while JUGADOR not in list:
-    JUGADOR = input('piedra, papel o tijera? :').lower()
-
-if JUGADOR == COMPUTADORA :
-     print("COMPUTADORA", COMPUTADORA)
-     print("JUGADOR" , JUGADOR)
+#buckle
+while True:
+    COMPUTADORA = random.choice(list)
+    JUGADOR = None
+#buckle
+    while JUGADOR not in list:
+        JUGADOR = input('piedra, papel o tijera? :').lower()
+#condiciones 
+    if JUGADOR == COMPUTADORA :
+     print("JUGADOR", JUGADOR)
+     print("COMPUTADORA" , COMPUTADORA)
      print("EMPATE!")
-elif JUGADOR == "piedra" :
-    if COMPUTADORA == "papel" :
-     print("COMPUTADORA", COMPUTADORA)
-     print("JUGADOR" , JUGADOR)
+    elif JUGADOR == "piedra" :
+     if COMPUTADORA == "papel" :
+      print("JUGADOR", JUGADOR)
+     print("COMPUTADORA" , COMPUTADORA)
      print("PERDISTE!")
-    if COMPUTADORA == "tijera" :
-     print("COMPUTADORA", COMPUTADORA)
-     print("JUGADOR" , JUGADOR)
-     print("GANASTE!")
+     if COMPUTADORA == "tijera" :
+         print("JUGADOR", JUGADOR)
+         print("COMPUTADORA" , COMPUTADORA)
+         print("GANASTE!")
+    elif JUGADOR == "papel" :
+     if COMPUTADORA == "piedra" :
+         print("JUGADOR", JUGADOR)
+         print("COMPUTADORA" , COMPUTADORA)
+         print("GANASTE!")
+     if COMPUTADORA == "tijera" :
+         print("JUGADOR", JUGADOR)
+         print("COMPUTADORA" , COMPUTADORA)
+         print("PERDISTE!")     
+    elif JUGADOR == "tijera" :
+     if COMPUTADORA == "piedra" :
+         print("JUGADOR", JUGADOR)
+         print("COMPUTADORA" , COMPUTADORA)
+         print("PERDISTE!")
+     if COMPUTADORA == "papel" :
+         print("JUGADOR", JUGADOR)
+         print("COMPUTADORA" , COMPUTADORA)
+         print("GANASTE!") 
+    Jugar_de_nuevo = input("QUIERES JUGAR DE NUEVO (SI,NO): ").lower()
+    if Jugar_de_nuevo != "si" :
+        break
+
+print("ADIOS")
+
+
+
      
